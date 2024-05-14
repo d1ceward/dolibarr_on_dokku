66498e640edddf82de13875bca89467fc0495711
Fabien LEFEBVRE (d1ceward) <contact@d1ceward.com>
2024-05-15 01:52:55 +0200

    Add missing database creation script

--- entrypoint.sh
+++ entrypoint.sh
@@ -72,8 +72,7 @@ EOF
   fi
 }
 
-function waitForDataBase()
-{
+waitForDataBase() {
   r=1
 
   while [[ ${r} -ne 0 ]]; do
@@ -86,12 +85,129 @@ function waitForDataBase()
   done
 }
 
+initializeDatabase() {
+  for fileSQL in /var/www/html/install/mysql/tables/*.sql; do
+    if [[ ${fileSQL} != *.key.sql ]]; then
+      echo "Importing table from `basename ${fileSQL}` ..."
+      sed -i 's/--.*//g;' ${fileSQL} # remove all comment
+      mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < ${fileSQL}
+    fi
+  done
+
+  for fileSQL in /var/www/html/install/mysql/tables/*.key.sql; do
+    echo "Importing table key from `basename ${fileSQL}` ..."
+    sed -i 's/--.*//g;' ${fileSQL}
+    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < ${fileSQL} > /dev/null 2>&1
+  done
+
+  for fileSQL in /var/www/html/install/mysql/functions/*.sql; do
+    echo "Importing `basename ${fileSQL}` ..."
+    sed -i 's/--.*//g;' ${fileSQL}
+    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < ${fileSQL} > /dev/null 2>&1
+  done
+
+  for fileSQL in /var/www/html/install/mysql/data/*.sql; do
+    echo "Importing data from `basename ${fileSQL}` ..."
+    sed -i 's/--.*//g;' ${fileSQL}
+    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < ${fileSQL} > /dev/null 2>&1
+  done
+
+  echo "Set some default const ..."
+  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "DELETE FROM llx_const WHERE name='MAIN_VERSION_LAST_INSTALL';" > /dev/null 2>&1
+  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "DELETE FROM llx_const WHERE name='MAIN_NOT_INSTALLED';" > /dev/null 2>&1
+  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "DELETE FROM llx_const WHERE name='MAIN_LANG_DEFAULT';" > /dev/null 2>&1
+  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "INSERT INTO llx_const(name,value,type,visible,note,entity) values('MAIN_VERSION_LAST_INSTALL', '${DOLIBARR_VERSION}', 'chaine', 0, 'Dolibarr version when install', 0);" > /dev/null 2>&1
+  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "INSERT INTO llx_const(name,value,type,visible,note,entity) VALUES ('MAIN_LANG_DEFAULT', 'auto', 'chaine', 0, 'Default language', 1);" > /dev/null 2>&1
+  mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "INSERT INTO llx_const(name,value,type,visible,note,entity) VALUES ('SYSTEMTOOLS_MYSQLDUMP', '/usr/bin/mysqldump', 'chaine', 0, '', 0);" > /dev/null 2>&1
+
+  echo "Enable user module ..."
+  php /var/www/scripts/docker-init.php
+
+  if [ -d /var/www/scripts/docker-init.d ] ; then
+    for file in /var/www/scripts/docker-init.d/*; do
+      [ ! -f $file ] && continue
+
+      # If extension is not in PHP SQL SH, we loop
+      isExec=$(echo "PHP SQL SH" | grep -wio ${file##*.})
+      [ -z "$isExec" ] && continue
+
+      echo "Importing custom ${isExec} from `basename ${file}` ..."
+      if [ "$isExec" == "SQL" ] ; then
+        sed -i 's/--.*//g;' ${file}
+        mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < ${file} > /dev/null 2>&1
+      elif [ "$isExec" == "PHP" ] ; then
+        php $file
+      elif [ "$isExec" == "SH" ] ; then
+        /bin/bash $file
+      fi
+    done
+  fi
+
+  # Update ownership after initialisation of modules
+  chown -R www-data:www-data /var/www/documents
+}
+
+migrateDatabase() {
+  TARGET_VERSION="$(echo ${DOLIBARR_VERSION} | cut -d. -f1).$(echo ${DOLIBARR_VERSION} | cut -d. -f2).0"
+  echo "Schema update is required ..."
+  echo "Dumping Database into /var/www/documents/dump.sql ..."
+
+  mysqldump -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} > /var/www/documents/dump.sql
+  r=${?}
+  if [[ ${r} -ne 0 ]]; then
+    echo "Dump failed ... Aborting migration ..."
+    return ${r}
+  fi
+  echo "Dump done ... Starting Migration ..."
+
+  echo "" > /var/www/documents/migration_error.html
+  pushd /var/www/htdocs/install > /dev/null
+  php upgrade.php ${INSTALLED_VERSION} ${TARGET_VERSION} >> /var/www/documents/migration_error.html 2>&1 && \
+  php upgrade2.php ${INSTALLED_VERSION} ${TARGET_VERSION} >> /var/www/documents/migration_error.html 2>&1 && \
+  php step5.php ${INSTALLED_VERSION} ${TARGET_VERSION} >> /var/www/documents/migration_error.html 2>&1
+  r=$?
+  popd > /dev/null
+
+  if [[ ${r} -ne 0 ]]; then
+    echo "Migration failed ... Restoring DB ... check file /var/www/documents/migration_error.html for more info on error ..."
+    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} < /var/www/documents/dump.sql
+    echo "DB Restored ..."
+    return ${r}
+  else
+    echo "Migration successful ... Enjoy !!"
+  fi
+
+  return 0
+}
+
+lockInstallation() {
+  touch /var/www/documents/install.lock
+  chown www-data:www-data /var/www/documents/install.lock
+  chmod 400 /var/www/documents/install.lock
+}
+
 run() {
   initDolibarr
   echo "Current Version is : ${DOLIBARR_VERSION}"
 
   if [[ ${DOLIBARR_INSTALL_AUTO} -eq 1 && ! -f /var/www/documents/install.lock ]]; then
     waitForDataBase
+
+    mysql -u ${DOLIBARR_DB_USER} -p${DOLIBARR_DB_PASSWORD} -h ${DOLIBARR_DB_HOST} -P ${DOLIBARR_DB_PORT} ${DOLIBARR_DB_NAME} -e "SELECT Q.LAST_INSTALLED_VERSION FROM (SELECT INET_ATON(CONCAT(value, REPEAT('.0', 3 - CHAR_LENGTH(value) + CHAR_LENGTH(REPLACE(value, '.', ''))))) as VERSION_ATON, value as LAST_INSTALLED_VERSION FROM llx_const WHERE name IN ('MAIN_VERSION_LAST_INSTALL', 'MAIN_VERSION_LAST_UPGRADE') and entity=0) Q ORDER BY VERSION_ATON DESC LIMIT 1" > /tmp/lastinstall.result 2>&1
+    r=$?
+    if [[ ${r} -ne 0 ]]; then
+      initializeDatabase
+    else
+      INSTALLED_VERSION=`grep -v LAST_INSTALLED_VERSION /tmp/lastinstall.result`
+      echo "Last installed Version is : ${INSTALLED_VERSION}"
+      if [[ "$(echo ${INSTALLED_VERSION} | cut -d. -f1)" -lt "$(echo ${DOLIBARR_VERSION} | cut -d. -f1)" ]]; then
+        migrateDatabase
+      else
+        echo "Schema update is not required ... Enjoy !!"
+      fi
+    fi
+
+    lockInstallation
   fi
 }
 
